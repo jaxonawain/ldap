fb9a9551384aef55f8768e827996a175950aadd8
Jaxonawain <jaxonawain@gmail.com>
2020-03-19 15:09:43 -0600

    asdf

--- gitpull.php
+++ gitpull.php
@@ -1,9 +1,7 @@
 <?php
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-
+    echo 'Current script owner: ' . get_current_user(); 
   shell_exec( 'cd /var/www/ldap/ldap/ && git reset --hard HEAD && git pull' );
-  
-
 }
 
 ?>hi hello 
