1476f96803cb125855e673062f9b7c965e1241ba
Jaxonawain <jaxonawain@gmail.com>
2020-03-19 13:46:14 -0600

    asdf

--- gitpull.php
+++ gitpull.php
@@ -1,6 +1,6 @@
 <?php
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/ldap/ldap/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd /var/www/ldap/ldap/ && git reset --hard HEAD && git pull 2>&1' );
 }
 
-?>hello motherfuckin world bitches try 3
+?>hello motherfuckin world bitches try 4
