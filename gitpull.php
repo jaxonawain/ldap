19e762b5d15d45bab9866a1ee33f892f987cb708
Jaxonawain <jaxonawain@gmail.com>
2020-03-19 14:08:48 -0600

    asdf

--- gitpull.php
+++ gitpull.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-  shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd /var/www/ldap/ldap/ && git reset --hard HEAD && git pull' );
 }
 
 ?>hi bitch
